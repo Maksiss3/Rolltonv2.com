048df1f0afce257959e286ea6cd756cb1f64903f
Maksiss <MaksimsJakovlevs1999@gmail.com>
2021-01-21 22:05:01 +0200

    Bugs

--- .js/order.js
+++ .js/order.js
@@ -1,3 +1,67 @@
+const nav_trigger = document.querySelector('.navTrigger');
+const upper = document.querySelector('i:nth-child(1)');
+const middle = document.querySelector('i:nth-child(2)');
+const lower = document.querySelector('i:nth-child(3)');
+
+const burger_menu = window.anime.timeline({
+  autoplay: false,
+  loop: false,
+});
+
+const burger_upper_combine = ({
+  targets: upper,
+  translateY: 8,
+  backgroundColor: '#FFEE00',
+  duration: 50,
+  easing: 'linear',
+});
+
+const burger_lower_combine = ({
+  targets: lower,
+  translateY: -8,
+  backgroundColor: '#FFEE00',
+  duration: 50,
+  easing: 'linear',
+});
+
+const fill_circle = ({
+  targets: nav_trigger,
+  backgroundColor: '#39a863',
+  duration: 50,
+})
+
+const to_X_1 =({
+  targets:[upper, middle],
+  rotate: 45,
+  backgroundColor: '#FFEE00',
+  duration: 50,
+  easing: 'linear',
+});
+
+const to_X_2 =({
+  targets: lower,
+  rotate: -45,
+  duration: 50,
+  easing: 'linear',
+});
+
+const nav_reveal =({
+  targets: '#navbar',
+  width: '80%',
+  translateX: -325,
+  duration: 50,
+  easing: 'linear',
+});
+
+burger_menu
+  .add(burger_upper_combine)
+  .add(burger_lower_combine,'-=50')
+  .add(fill_circle)
+  .add(to_X_1)
+  .add(to_X_2, '-=50')
+  .add(nav_reveal)
+
+
 // Price animation
 previous_total = 0;
 function generatePrice(button){
@@ -17,6 +81,24 @@ function numberAnimation(previous_total, total){
     duration: 2000,
   });
 }
+
+var mq = window.matchMedia( "(max-width: 600px)" );
+  if (mq.matches) { // window width is at less than 600px
+    nav_trigger.addEventListener('click', ()=>{
+      if (!nav_trigger.classList.contains('active')){
+        nav_trigger.classList.add('active');
+        burger_menu.play();
+        burger_menu.reverse();
+      }else if(nav_trigger.classList.contains('active')){
+        nav_trigger.classList.remove('active');
+        burger_menu.play();
+        burger_menu.reverse();
+      }})
+
+
+
+
+
 
 buttons = document.querySelectorAll('.pieces_buttons > button');
 function removeClicked(){
@@ -51,6 +133,7 @@ function startAnimation(order){
   var mq = window.matchMedia( "(max-width: 600px)" );
   if (mq.matches) { // window width is at less than 600px
     truckAnimationMobile();
+
   }else { // window width is greater than 600px
     truckAnimation();
   }
@@ -193,4 +276,4 @@ function truckAnimation(){
   }
 
 
- 
+  }
